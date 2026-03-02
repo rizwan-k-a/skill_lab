435c88c054463cc74c6b3804327b5361f2714c5c
S Abhilash <sabhilash.mca25@rvce.edu.in>
2026-03-02 14:01:12 +0530

    updated 2nd time

--- program.py
+++ program.py
@@ -1,9 +1,10 @@
 # Initialize 3 empty partitions
-p1, p2, p3 = [], [], []
+p1, p2, p3, p4 = [], [], [], []
 
 # "Push" data into them
 p1.append("S Abhilash")
 p2.append("Rizwan K A")
-p3.append("Rashmi Deepak Naik")
+p3.append("Rashmi")
+p4.append("Unknown")
 
-print(f"Part 1: {p1}, Part 2: {p2}, Part 3: {p3}")
+print(f"Part 1: {p1}, Part 2: {p2}, Part 3: {p3}, Part 4: {p4}")
