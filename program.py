97d7c0f636bf3e6c7a63a1950c09a33225531598
rizwan-k-a <rizwanka.professional@gmail.com>
2026-03-02 13:29:40 +0530

    program.py

---
+++ program.py
@@ -0,0 +1,9 @@
+# Initialize 3 empty partitions
+p1, p2, p3 = [], [], []
+
+# "Push" data into them
+p1.append("User A")
+p2.append("User B")
+p3.append("User C")
+
+print(f"Part 1: {p1}, Part 2: {p2}, Part 3: {p3}")
